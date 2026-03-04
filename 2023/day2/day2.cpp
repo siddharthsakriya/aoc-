66d7938eda5d3c5d54428f1bf79c5ecbfdb19a1b
Siddharth Sakriya <siddharthsakriya@Siddharths-MacBook-Pro.local>
2026-03-04 20:30:48 +0000

    this needs refactored at some point lol

--- 2023/day2/day2.cpp
+++ 2023/day2/day2.cpp
@@ -31,22 +31,66 @@ void partOne() {
     std::vector<std::string> inputs = getInputs();
     int total = 0;
 
-    std::vector<std::vector<std::string>> colon_delim;
     std::string seg;
 
     for (std::string input : inputs) {
+        
         std::stringstream curr(input);
         std::vector<std::string> split;
+
         while (std::getline(curr, seg, ':')) {
             split.push_back(seg);
         }
 
-        std::cout << split[0] << "\n" << split[1] << "\n";
+        std::string game_num = split[0];
+        std::string converted;
+        
+        for (auto &ch : game_num) {
+            if (std::isdigit(ch)) {
+                converted.append(std::string(1, ch));
+            }
+        }
+
+        int int_value = stoi(converted);
+
+        std::string seg2;
+        std::stringstream curr2 (split[1]);
+        std::vector<std::string> split2;
+
+        while (std::getline(curr2, seg2, ';')) {
+            split2.push_back(seg2);
+        }
+        
+        std::string seg3;
+        std::vector<std::string> individual_picks;
+        std::vector<std::string> split3;
+        for (const std::string& s : split2) {
+            std::stringstream curr3 (s);
+            while (std::getline(curr3, seg3, ',')) {
+                split3.push_back(seg3);
+            }
+        }    
+        
+
+        bool valid = true;
+
+        for (const std::string& s : split3) {
+            std::stringstream ss(s);
+            int num;
+            std::string colour;
+
+            ss >> num >> colour;
+
+            if (colour == "red" && num > 12) {valid = false;}
+            if (colour == "green" && num > 13) {valid = false;}
+            if (colour == "blue" && num > 14) {valid = false;}
+        }
+        
+        if (valid) {
+            total += int_value;
+        }
     }
-
-    
-
-
+    std::cout << "The answer for Part 1 is " << total << "\n"; 
 }
 
 int main() {
